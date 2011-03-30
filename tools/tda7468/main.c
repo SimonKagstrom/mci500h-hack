e4e18154cc50f0480f98796d409a412624ad0fa1
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-03-30 07:21:14 +0200

    tools: TDA7468 tool: More work in progress

--- tools/tda7468/main.c
+++ tools/tda7468/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,6 +8,12 @@
 #include <fcntl.h>
 
 #include <linux/i2c-dev.h>
+
+#define panic(fmt...) do { \
+	fprintf(stderr, "%s: ", __FUNCTION__); \
+	fprintf(stderr, fmt); \
+	exit(1); \
+} while (0)
 
 enum tda7468d_functions {
 	FUNC_INPUT_SELECT_AND_MIC = 0,
@@ -76,8 +83,8 @@ union tda7468d_bass_alc {
 	uint8_t b;
 };
 
-static void tda7468d_write(enum tda7468d_functions fd,
-		uint8_t func, uint8_t val)
+static void tda7468d_write(int fd,
+		enum tda7468d_functions func, uint8_t val)
 {
 	char buf[2];
 	int r;
@@ -86,13 +93,84 @@ static void tda7468d_write(enum tda7468d_functions fd,
 	buf[1] = val;
 
 	r = write(fd, buf, sizeof(buf));
-	if (r < 0) {
-		fprintf(stderr, "write error: %d\n", r);
-		exit(1);
-	}
+	if (r < 0)
+		panic("write error: %d\n", r);
 }
 
+static void do_input(int fd, int n_opts, const char **options)
+{
+	union tda7468d_input_and_mic v;
+	int i;
 
+	v.b = 0;
+	v.bits.mic_off = 1;
+
+	for (i = 0; i < n_opts; i++) {
+		const char *p = options[i];
+
+		if (strcmp(p, "in1") == 0)
+			v.bits.input = 0;
+		else if (strcmp(p, "in2") == 0)
+			v.bits.input = 1;
+		else if (strcmp(p, "in3") == 0)
+			v.bits.input = 2;
+		else if (strcmp(p, "in4") == 0)
+			v.bits.input = 3;
+
+		else if (strcmp(p, "mute") == 0)
+			v.bits.mute_on = 1;
+		else if (strcmp(p, "unmute") == 0)
+			v.bits.mute_on = 0;
+
+		else if (strcmp(p, "mic_0") == 0) {
+			v.bits.mic_off = 0;
+			v.bits.mic_gain = 3;
+		}
+		else if (strcmp(p, "mic_6") == 0) {
+			v.bits.mic_off = 0;
+			v.bits.mic_gain = 2;
+		}
+		else if (strcmp(p, "mic_10") == 0) {
+			v.bits.mic_off = 0;
+			v.bits.mic_gain = 1;
+		}
+		else if (strcmp(p, "mic_14") == 0) {
+			v.bits.mic_off = 0;
+			v.bits.mic_gain = 0;
+		}
+	}
+
+	tda7468d_write(fd, FUNC_INPUT_SELECT_AND_MIC, v.b);
+}
+
+static void do_gain(int fd, int n_opts, const char **options)
+{
+	panic("NYI\n");
+}
+
+static void do_volume(int fd, int is_left, int n_opts, const char **options)
+{
+}
+
+static void do_output(int fd, int n_opts, const char **options)
+{
+	panic("NYI\n");
+}
+
+static void do_surround(int fd, int n_opts, const char **options)
+{
+	panic("NYI\n");
+}
+
+static void do_treble_bass(int fd, int n_opts, const char **options)
+{
+	panic("NYI\n");
+}
+
+static void do_bass_alc(int fd, int n_opts, const char **options)
+{
+	panic("NYI\n");
+}
 
 static int open_i2c(const char *dev, int client_addr)
 {
@@ -110,7 +188,55 @@ static int open_i2c(const char *dev, int client_addr)
 	return fd;
 }
 
+static void close_i2c(int fd)
+{
+	close(fd);
+}
+
+static void usage(void)
+{
+	printf("Usage: tda7468-tool <command> [options]\n\n"
+			"Where <command> and [options] can be\n\n"
+			""
+			);
+	exit(1);
+}
+
 int main(int argc, const char *argv[])
 {
+	const char *command;
+	const char **options;
+	int n_opts;
+	int fd;
+
+	if (argc < 4)
+		usage();
+	command = argv[1];
+	options = &argv[2];
+	n_opts = argc - 2;
+
+	fd = open_i2c("/dev/i2c-1", 0x44);
+	if (fd < 0)
+		panic("Can't open I2C device\n");
+
+	if (strcmp(command, "input") == 0)
+		do_input(fd, n_opts, options);
+	else if (strcmp(command, "gain") == 0)
+		do_gain(fd, n_opts, options);
+	else if (strcmp(command, "surround") == 0)
+		do_surround(fd, n_opts, options);
+	else if (strcmp(command, "volume-left") == 0)
+		do_volume(fd, 1, n_opts, options);
+	else if (strcmp(command, "volume-right") == 0)
+		do_volume(fd, 0, n_opts, options);
+	else if (strcmp(command, "treble") == 0)
+		do_treble_bass(fd, n_opts, options);
+	else if (strcmp(command, "output") == 0)
+		do_output(fd, n_opts, options);
+	else if (strcmp(command, "bass-alc") == 0)
+		do_bass_alc(fd, n_opts, options);
+
+	close_i2c(fd);
+
 	return 0;
 }
